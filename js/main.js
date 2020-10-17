142bac5b1bdc569056a7a2c27c48dc4ebbdda286
Selena Wells <selena.elise.wells@gmail.com>
2020-10-16 22:37:05 -0400

    about us styling added no bugs so far

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(() => {
+
+
+    let mainNav = document.getElementById('mainNav');
+    let navBarToggle = document.getElementById('js-navbar-toggle');
+
+    navBarToggle.addEventListener('click', function() {
+
+        console.log('she works');
+
+        mainNav.classList.toggle('selected');
+    });
+
+
+
+
+})();
